d434d5f3ec9e1de928cf0712448c198658b31ddf
Allan Oliveira <allanolme@gmail.com>
2023-08-13 16:01:36 -0300

    Update serve.js

--- nodeJs'API'/serve.js
+++ nodeJs'API'/serve.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const app = express();
+
 app.listen("3000");
+app.route("/").get( (req, res) => {
+	res.send("hello");
+})
+app.route("/sobre").get( (req, res) => {
+	res.send("hello sobre");
+})
