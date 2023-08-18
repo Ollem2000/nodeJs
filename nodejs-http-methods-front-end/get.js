23b347d2f637774eb9db2dff11d9aa1f93ccc08c
Allan Oliveira <allanolme@gmail.com>
2023-08-17 22:19:46 -0300

    Create get

---
+++ nodejs-http-methods-front-end/get.js
@@ -0,0 +1,7 @@
+//const url = "http://localhost.5500/api";
+const url = "https://api.github.com/users/ollem2000";
+
+function getUsers(){
+	fetch(url).then(r => r.json()).then(data => renderApiResult.textContent = JSON.stringify(data)).catch(e => console.log(e))
+}
+getUsers();
